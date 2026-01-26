6437ef2258ca088864c42bd76d74e687bc7e466d
Sibasundar Barik <bariksiba33@gmail.com>
2026-01-26 13:19:34 +0530

    Add files via upload

---
+++ Day 2 - Variables and Command-Line Arguments/Sample2.sh
@@ -0,0 +1,9 @@
+
+ #!/bin/bash
+ echo "Script name: $0"
+ echo "First argument: $1"
+ echo "Second argument: $2"
+ echo "Total arguments: $#"
+ echo "All arguments: $@"
+ echo "Last Command Status: $?"
+ echo "Script PID: $$"
